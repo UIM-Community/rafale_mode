025d35e4b5d37cc487399cfa522f8a98fc00132c
Gentilhomme <gentilhomme.thomas@gmail.com>
2017-02-19 20:24:10 +0100

    Update comment & remove log method

--- rafale_mode.lua
+++ rafale_mode.lua
@@ -1,22 +1,7 @@
----------------------------------------------------------------------
--- Permet d'avoir un fichier log si inexistant et d'écrire dedans
----------------------------------------------------------------------
-function log(text)
-  fname = "E:\\Program Files (x86)\\Nimsoft\\probes\\service\\nas\\scripts\\rafale-mode\\rafale-mode_capture.log"
-  local fl = file
-  local ts = timestamp
-  b,nread = fl.read(fname)
-  if b == nil then
-    fl.create(fname)
-  end
-  time = ts.format( ts.now (),"%c" )
-  fl.write(fname,time.." - "..text.."\n")
-end
 
 -------------------------------------------
--- Extrait la rafale
+-- Extract rafal parameters from the alarm message
 -------------------------------------------
--- # : devant une variable tableau permet de reprendre la taille du tableau
 function getRafale(text)
     local firstSplit = split(text, "]")
     local secondSplit = split(firstSplit[#firstSplit], "[")
@@ -26,19 +11,16 @@ function getRafale(text)
   return split(secondSplit[2],",")
 end
 
--- Gain de performance
+-- Chunk optimization (processing up to 10%+ over memory)
 al  = alarm
 db  = database
 bus = nimbus
 act = action
 
--- reprendre l'alarme
+-- Get alarm
 a = al.get()
-log("-------------------------------------------------")
 
 if a ~= nil then
-  log("-------------------------------------------------")
-  log("Ouverture de l'alarme avec l'id => "..tostring(a.message))
   rafale = getRafale(a.message)
 
   -- Reprend les parametres de la rafale entre crochet
@@ -69,26 +51,22 @@ if a ~= nil then
       -- on lance une alarme via nimalame.exe en commande et on supprime les résultat identique en base.
       -- sinon on ajoute l'alarme en base
       if #result>=tonumber(occur) then
-        log("Nous somme en condition d'alarme ")
 
         -- Récupération du robotname dans la base de donnée
         local hostname
         for k, v in pairs(result[1]) do
-          --log(" K : " ..tostring(k))
-          --log(" V : " ..tostring(v))
           if tostring(k) == "hostname" then
             hostname = tostring(v)
             break
           end
         end
-        log("Hostname : " .. tostring(hostname))
         local alarme = "nimalarm.exe -i -l ".. level .." -S ".. a.source .." \"".. tmpmsg .." - nb: ".. #result ..">=" ..occur.. " in "..scope.."sec.\""
         local temp,rc = act.command(alarme)
-        -- si la commande nimalarm est ok, on supprime en base
+        
+        -- if nimAlarm return NimOK we delete the entry in the database!
         if temp ~= nil and rc == 0 then
           local delete = "DELETE FROM events WHERE source='".. a.source .."' AND suppkey='".. a.supp_key .."' AND sid='".. a.sid .."'"
           db.query(delete)
-          log("Requête SQL : delete => "..delete);
           act.close(a.nimid)
         else
           bus.alarm(level,"Message d'Alarme pour indiquer que le code a planté")
@@ -107,16 +85,12 @@ if a ~= nil then
       local insert = "INSERT INTO events(nimid,nimts,source,hostname,origin,sid,message,scope,occur,level,suppkey) "
       local values = "VALUES ('".. a.nimid .."','".. a.nimts .."','".. a.source .."','".. a.hostname .."','".. a.origin .."','".. a.sid .."','".. newmsg .."','".. scope .."','".. occur .."','".. level .."','"..a.supp_key.."')"
       db.query (insert..values)
-      log("Requête SQL => "..insert..values)
       act.close(a.nimid)
     end
     db.close()
   else
-    -- si on n'a pas de SUPP_Key ou de SubsystemID(nombre)
+    -- If we have no supp_key or subsystem_id(number)
     local alarme = "nimalarm.exe -i -l ".. level .." -S ".. a.source .." \"don't find supp_key or sid : ".. a.message.." \""
     act.command(alarme)
-    log("Information => pas de SUPP_KEY et/ou SUBSYS : " .. a.message)
   end
-else
-  log("Information => Aucune alarme détectée!")
 end
